6cc4c3fa2f6b8e3a2828d307825d9a3cb007786e
강경만(LGU/아이들나라서비스개발팀/인턴) <103920436+kyeongmankang@users.noreply.github.com>
2022-04-22 17:15:26 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import './index.css';
 const div = document.createElement('div');
 const msg = 'Boilerplate';
 const eslint = ''; // lint problems -> error: no-unused-vars
+var hi;
+let a;
+const a;
 
 div.innerText = msg;
 document.querySelector('body').appendChild(div);
